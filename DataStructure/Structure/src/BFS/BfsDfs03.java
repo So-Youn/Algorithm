f8b9b912b8ed44bc71c161c078be6a1b77a7111a
netComm <soyun3963@gmail.com>
2020-10-01 00:24:31 +0900

    0930 dfs 수정

--- DataStructure/Structure/src/BFS/BfsDfs03.java
+++ DataStructure/Structure/src/BFS/BfsDfs03.java
@@ -1,42 +1,70 @@
 package BFS;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-//단지 번호  붙이기
+
+//단지 번호  붙이기(https://www.acmicpc.net/problem/2667)
+
 public class BfsDfs03 {
 	static int N;
 	static StringTokenizer st;
 	static int[][] arr;
-	static boolean[][] checked;
+
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
+
 	static int count;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1][N + 1];
-		checked = new boolean[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
+		arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
-			for (int j = 1; j <= N; j++) {
-				arr[i][j] = input.charAt(j - 1) - '0';
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = input.charAt(j) - '0';
 			}
 		}
-		dfs(1, 1);
-	}
+		// 오름차순으로 출력하기 위해 PriorityQueue에 저장함
+		int total = 0;
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 
-	private static void dfs(int i, int j) {
-		checked[i][j] = true;
-		for (i = 1; i <= i; i++) {
-			for (j = 1; j <= N; j++) {
-				if (arr[i][j] == 1 && checked[i][j] == false) {
-					// 다음 연결점을 찾아 시작점을 변경하여 재호출
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (arr[i][j] == 1) {
+					count = 0;
+					total++;
 					dfs(i, j);
+					pq.add(count);
 				}
 			}
-			System.out.println(count);
+		}
+		bw.write(total + "\n");
+
+		while (!pq.isEmpty())
+			bw.write(pq.poll() + "\n");
+		bw.flush();
+
+	}
+
+	private static void dfs(int x, int y) {
+		arr[x][y] = 0;
+		count++;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+				if (arr[nx][ny] == 1)
+					dfs(nx, ny);
+			}
 		}
 
 	}
--- DataStructure/Structure/src/BFS/BfsDfs03_2.java
+++ DataStructure/Structure/src/BFS/BfsDfs03_2.java
@@ -1,13 +1,59 @@
 package BFS;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BfsDfs03_2 {
-	static int N;
+	static int N, count;
+	static int[][] arr;
+	static int[][] visited;
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
+
+	static ArrayList<Integer> al = new ArrayList<>();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
+		N = sc.nextInt();
 
+		arr = new int[N][N];
+		visited = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String input = sc.next();
+			for (int j = 0; j < N; j++)
+				arr[i][j] = input.charAt(j) - '0';
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				if (arr[i][j] == 1 && visited[i][j] == 0) {
+					count = 1;
+					dfs(i, j);
+					al.add(count);
+				}
+		}
+		Collections.sort(al);
+		System.out.println(al.size());
+		for (int i = 0; i < al.size(); i++) {
+			System.out.println(al.get(i));
+		}
+	}
+
+	static int dfs(int x, int y) {
+		visited[x][y] = 1;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+				if (arr[nx][ny] == 1 && visited[nx][ny] == 0) {
+					dfs(nx, ny);
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 }
